094900d77a2b31e80d8e008125118194ec3500b4
mrksss8 <devmrksss@gmail.com>
2023-02-16 12:20:34 +0800

    modified - analytics add graph

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -4,21 +4,83 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Treatment;
+use App\Models\TreatmentMedicine;
+use App\Models\Medicine;
+use Carbon\Carbon;
 
 class AnalyticsController extends Controller
 {
     public function index(){
         
-         $datas = DB::table('medicines')->get('*')->toArray();
+        //  $datas = DB::table('medicines')->get('*')->toArray();
 
-            foreach($datas as $data){
+        //     foreach($datas as $data){
                 
-                $dataPoints[] = array(
-                    'label'=> $data->brand_name,
-                    'y'=>$data->stocks,
-                );
+        //         $dataPoints[] = array(
+        //             'label'=> $data->brand_name,
+        //             'y'=>$data->stocks,
+        //         );
+        //     }
+
+        // 
+
+        //global variable to get last 7 days 
+        $date = Carbon::today()->subDays(7);
+        $treatments = Treatment::where('created_at','>=',$date)->get()->groupBy(function($d) {
+                return Carbon::parse($d->created_at)->format('Y-m-d');
+                });
+
+            foreach($treatments as $treatment=>$record){
+            //  echo $treatment;
+               
+               $count = count($record);
+            //  echo $count;
+
+            //     $data[] = array(
+            //     'date' => $treatment,
+            //     'count' => $count,
+            //    );
+
+                $dataLabels[] = $treatment;
+                $dataPoints[] = $count;
             }
 
-        return view('analytics.index',compact('dataPoints'));
+             $treatment_medicines = TreatmentMedicine::where('created_at','>=',$date)->get()->groupBy(function($q) {
+                 return Carbon::parse($q->created_at)->format('Y-m-d');
+                 });
+            
+             foreach($treatment_medicines as $treatment_medicine=>$records){
+                 $med_count = 0;
+                 foreach ($records as $key => $record){
+                     $med_count = $med_count + $record->quantity;
+
+                    }
+                     $dataPoints_treat_med[] = $med_count;
+                     $dataLabels_treat_med[] = $treatment_medicine;
+            }
+
+            // $med = DB::table('treatment_medicines')
+            //      ->select('quantity')
+            //      ->groupBy('medicine_id')
+            //      ->get();
+            
+            // $med = TreatmentMedicine::with('medicine')->groupBy('medicine_id')->count();
+
+            // $med  = DB::table('treatment_medicines')
+            //      ->select('quantity', DB::raw('count(*) as total'))
+            //      ->groupBy('medicine_id')
+            //      ->get();
+
+            $med_count_by_categories = TreatmentMedicine::groupBy('category')->where('created_at','>=',Carbon::today()->subDays(30))
+            ->selectRaw('category, sum(quantity) as sum')
+            ->get();
+
+             foreach($med_count_by_categories as $med_count_by_category){
+                    $dataPoints_med_count_by_category[] = $med_count_by_category->sum;
+                    $dataLabels_med_count_by_category[] = $med_count_by_category->category;
+
+                }
+        return view('analytics.index',compact('dataLabels','dataPoints','dataPoints_treat_med','dataLabels_treat_med','dataPoints_med_count_by_category','dataLabels_med_count_by_category'));
     }
 }
--- resources/views/analytics/index.blade.php
+++ resources/views/analytics/index.blade.php
@@ -23,74 +23,176 @@
         </div>
     @endif
 
-    <div class="row d-flex justify-content-center">
+    {{-- <div class="row mb-2">
         <div class="col-md-6">
-            <div id="chartContainer" style="height: 400px; width: 100%;"></div>
+            <canvas id="given_med_count_by_category" class="border border-primary"></canvas>
         </div>
+    </div> --}}
+
+    <div class="row">
         <div class="col-md-6">
-            <div id="chartContainers" style="height: 400px; width: 100%;"></div>
+            <div class="row d-flex flex-col">
+                <div class="col-md-12 mb-2">
+                    <canvas id="given_med_count_by_category" class="border border-primary"></canvas>
+                </div>
+                <div class="col-md-12 mb-2">
+                    {{-- <canvas id="numTreated_lastSevenDays" class="border border-primary"></canvas> --}}
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="row d-flex flex-col">
+                <div class="col-md-12 mb-2">
+                    <canvas id="numtreatment_medicine_lastSevenDays" class="border border-primary"></canvas>
+                </div>
+                <div class="col-md-12 mb-2">
+                    <canvas id="numTreated_lastSevenDays" class="border border-primary"></canvas>
+                </div>
+            </div>
         </div>
     </div>
+
+
+
+
 
 @endsection
 
 @section('scripts')
-    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js"
+        integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <script>
-        window.onload = function() {
-
-            var chart = new CanvasJS.Chart("chartContainer", {
-                animationEnabled: true,
-                exportEnabled: true,
-                title: {
-                    text: "Medicines Stocks",
-                    fontFamily: "poppins",
-                    fontSize: 18,
-                },
-                axisY: {
-                    includeZero: true,
-
-                },
-                data: [{
-                    fontFamily: "poppins",
-                    type: "pie",
-                    indexLabel: "{y}",
-                    indexLabelPlacement: "inside",
-                    indexLabelFontWeight: "bolder",
-                    indexLabelFontColor: "white",
-                    // indexLabel: "{label} - #percent%",
-                    // yValueFormatString: "฿#,##0",
-                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+        const ctx = document.getElementById('numTreated_lastSevenDays');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: <?php echo json_encode($dataLabels); ?>,
+                datasets: [{
+                    label: 'No. of Treatment for the last 7 days',
+                    data: <?php echo json_encode($dataPoints); ?>,
+                    borderWidth: 1,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
                 }]
-            });
-            chart.render();
+            },
+            plugins: [ChartDataLabels],
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
 
-            var charts = new CanvasJS.Chart("chartContainers", {
-                animationEnabled: true,
-                exportEnabled: true,
-                title: {
-                    text: "Medicines Stocks",
-                    fontFamily: "poppins",
-                    fontSize: 18,
-                },
-                axisY: {
-                    includeZero: true,
+                }
+            }
+        });
 
-                },
-                data: [{
-                    fontFamily: "poppins",
-                    type: "column",
-                    indexLabel: "{y}",
-                    indexLabelPlacement: "inside",
-                    indexLabelFontWeight: "bolder",
-                    indexLabelFontColor: "white",
-                    // indexLabel: "{label} - #percent%",
-                    // yValueFormatString: "฿#,##0",
-                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+        const cty = document.getElementById('numtreatment_medicine_lastSevenDays');
+        new Chart(cty, {
+            type: 'bar',
+            data: {
+                labels: <?php echo json_encode($dataLabels_treat_med); ?>,
+                datasets: [{
+                    label: 'No. of given medicine for the last 7 days',
+                    data: <?php echo json_encode($dataPoints_treat_med); ?>,
+                    borderWidth: 1,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
                 }]
-            });
-            charts.render();
+            },
+            plugins: [ChartDataLabels],
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
 
-        }
+                }
+            }
+        });
+        const ctz = document.getElementById('given_med_count_by_category');
+        new Chart(ctz, {
+            type: 'doughnut',
+            data: {
+                labels: <?php echo json_encode($dataLabels_med_count_by_category); ?>,
+                datasets: [{
+                    label: 'No. given medicine for the past 30 days',
+                    data: <?php echo json_encode($dataPoints_med_count_by_category); ?>,
+                    borderWidth: 1,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
+                }]
+            },
+            plugins: [ChartDataLabels],
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'No. of given medicine by category for past 30 days'
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+
+                },
+            }
+        });
     </script>
 @endsection
