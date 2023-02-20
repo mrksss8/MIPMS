b2c61a06a894c3daac62c02e747c978efbe0f1b0
Mark Anthony N. Bautista <associate.local07@gmail.com>
2023-02-20 19:39:03 +0800

    change - name of app in sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -57,7 +57,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     {{-- <i class="fas fa-laugh-wink"></i> --}}
                 </div>
-                <div class="sidebar-brand-text mx-3">PMIS</div>
+                <div class="sidebar-brand-text mx-3"> MIPMS - Bay Municipal Clinic</div>
             </a>
 
 
