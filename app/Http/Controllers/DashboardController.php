c8368fa34533c2cb56df254b2968539eaafe4f7a
Mark Anthony N. Bautista <associate.local07@gmail.com>
2023-03-27 12:48:33 +0800

    modified - dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -25,6 +25,45 @@ class DashboardController extends Controller
 
         $treatmentedPatientToday_count = Treatment::whereDate('created_at', Carbon::today())->count();
 
-        return view('home', compact('patient_count', 'forTreatment_count', 'criticalMedicine_count', 'treatmentedPatientToday_count'));
+        //patientSexCount
+        $patientMaleCount = Patient::where('sex', 'male')->count();
+        $patientFemaleCount = Patient::where('sex', 'female')->count();
+
+        //patientAgeCount
+        $newborns_Cnt = 0;
+        $infants_Cnt = 0;
+        $children_Cnt = 0;
+        $adolescents_Cnt = 0;
+        $adults_Cnt = 0;
+        $senior_Cnt = 0;
+
+        $newborns_Cnt = 0;
+        $infants_Cnt = 0;
+        $children_Cnt = 0;
+        $adolescents_Cnt = 0;
+        $adults_Cnt = 0;
+        $senior_Cnt = 0;
+
+        $patients = Patient::all();
+        foreach ($patients as $patient) {
+            $ageInMonths = Carbon::parse($patient->birth_date)->diffInMonths(Carbon::now());
+
+            if (Carbon::parse($patient->birth_date)->diffInDays(Carbon::now()) <= 30) {
+                $newborns_Cnt++;
+            } elseif ($ageInMonths > 0 && $ageInMonths <= 12) {
+                $infants_Cnt++;
+            } elseif ($ageInMonths > 12 && $ageInMonths <= 216) {
+                $children_Cnt++;
+            } elseif ($ageInMonths > 216 && $ageInMonths <= 204) {
+                $Adolescents_Cnt++;
+            } elseif ($ageInMonths > 204 && $ageInMonths < 720) {
+                $adults_Cnt++;
+            } elseif ($ageInMonths >= 720) {
+                $senior_Cnt++;
+            }
+        }
+
+
+        return view('home', compact('patient_count', 'forTreatment_count', 'criticalMedicine_count', 'treatmentedPatientToday_count', 'patientMaleCount', 'patientFemaleCount', 'newborns_Cnt', 'infants_Cnt', 'adolescents_Cnt', 'children_Cnt', 'adults_Cnt', 'senior_Cnt'));
     }
 }
